ff7e403985c767880268a0b3f71afcf1fdd869cc
Kimmiejj <kin59153@gmail.com>
2026-07-20 21:43:43 +0700

    Connect deployed Google Apps Script endpoint

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // วาง URL ของ Google Apps Script Web app ที่นี่หลังจาก Deploy แล้ว
-window.SDS_CONFIG = { apiUrl: "" };
+window.SDS_CONFIG = { apiUrl: "https://script.google.com/macros/s/AKfycbzDaO52zheLTgFzIbvqG3D7-TMMUAgcnyfRFMybwYppXCKIqicAFoUqDUKQkjw80PP_BA/exec" };
